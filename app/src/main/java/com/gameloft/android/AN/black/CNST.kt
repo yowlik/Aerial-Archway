1ca421b9d2864fcbf55f4c53e82a2ebcf195be85
yowlik <yowl609@gmail.com>
2022-08-17 12:15:13 +0300

    test

---
+++ app/src/main/java/com/gameloft/android/AN/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.gameloft.android.AN.black
+
+object CNST {
+    const val AF_DEV_KEY = "ipnxCfPUJzZHVHWApdXoc"
+    const val jsoupCheck = "8f1c"
+    const val ONESIGNAL_APP_ID = "7b11fa7f-b471-4e5c-afc6-3add2b950293"
+
+    var lru = "http://aerialarchway.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+    val twoSub = "sub_id_2="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+    var DEV: String = "false"
+}
